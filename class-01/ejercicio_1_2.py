5cb7492237bba95804a310bb1f9b1631bdc7d332
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-13 07:32:19 -0500

    feat: exercises of class 1 finished

---
+++ class-01/ejercicio_1_2.py
@@ -0,0 +1,17 @@
+# Pedirle al usuario que ingrese 2 número enteros y que devuelva la suma, la multiplicación
+# la resta y division
+
+# una pequeña ayuda
+numero_a = int(input("Ingrese un número entero: "))
+numero_b = int(input("Ingrese otro número entero: "))
+
+suma = numero_a + numero_b
+resta = numero_a - numero_b
+multiplicacion = numero_a * numero_b
+division = numero_a / numero_b
+
+print(f"La suma de ambos números es: {suma}")
+print(f"La resta de ambos números es: {resta}")
+print(f"La multiplicacion de ambos números es: {multiplicacion}")
+print(f"La division de ambos números es: {division}")
+
