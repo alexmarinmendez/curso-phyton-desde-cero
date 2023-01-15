dff6155dfa229c19e1d12f2be75750ae9d7a11cc
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-15 12:05:47 -0500

    feat: Add extra exercises (ex2) in class 2

--- class-02/ejercicios_extra.py
+++ class-02/ejercicios_extra.py
@@ -7,3 +7,12 @@
 
 subjects = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
 print(subjects)
+
+# Ejercicio 2
+# Escribir un programa que almacene las asignaturas de un curso
+# (por ejemplo Matemáticas, Física, Química, Historia y Lengua)
+# en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>,
+# donde <asignatura> es cada una de las asignaturas de la lista.
+
+for subject in subjects:
+    print("Yo estudio " + subject)
