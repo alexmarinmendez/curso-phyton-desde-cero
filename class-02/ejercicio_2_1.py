2c96220b68e465d28a8ed04aee9b32bfefaa51fd
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-16 06:08:41 -0500

    feat: solve exercises 1 - 3 in ejercicios_2_1.py

--- class-02/ejercicio_2_1.py
+++ class-02/ejercicio_2_1.py
@@ -1,12 +1,27 @@
 """ Usando el formulario creado en la primera clase """
 
+nombre = input("Cual es su nombre? ")
+apellido = input("Cual es su apellido? ")
+edad = input("Cual es su edad: ")
 
 # 1- Crear una lista con esos datos y mostrar por consola 
+lista = [nombre, apellido, edad]
+print(f'La lista de datos es {lista}')
 
 # 2- Crear un diccionario poniendo como clave lo que represeta cada valor
+diccionario = {
+    'nombre': nombre,
+    'apellido': apellido,
+    'edad' : edad
+}
+print(f'El diccionario de datos es {diccionario}')
 
 # 3- Solicitar al usuario ingresar 3 datos por consola y luego insertarlos en un conjunto 
 # para mostrarlos por pantalla
+# conjunto = {nombre, apellido, edad}
+conjunto = set(lista)
+print(f'El conjunto de datos es {conjunto}')
+
 
 """ DICCIONARIOS ANIDADOS """
 
@@ -33,7 +48,7 @@ alumnos = {
 alumnos['alumno_1'] = alumno_1
 alumnos.update({'alumno_2': alumno_2})
 
-print(alumnos) # {'alumno_1': {'Lengua': 9, 'Matemática': 7, 'Informática': 7}, 'alumno_2': {'Lengua': 3, 'Matemática': 4, 'Informática': 5}}
+# print(alumnos) # {'alumno_1': {'Lengua': 9, 'Matemática': 7, 'Informática': 7}, 'alumno_2': {'Lengua': 3, 'Matemática': 4, 'Informática': 5}}
 
 # alumno_3 
 
