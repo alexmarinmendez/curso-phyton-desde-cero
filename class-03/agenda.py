181c2ff321869948bf54756a1182af2c1367a7d5
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-22 11:54:06 -0500

    feat: add agregar_contacto

--- class-03/agenda.py
+++ class-03/agenda.py
@@ -6,9 +6,30 @@ menu = {
     5: "Buscar un contacto",
     6: "Salir"
 }
+contactos = {}
 
 def mostrar_diccionario(diccionario: dict):
     for x, y in diccionario.items():
         print(f"{x}: {y}")
 
-mostrar_diccionario(menu)
+def comprobar_opcion(opcion):
+    if opcion>=1 and opcion<=6:
+        if opcion == 1:
+            agregar_contacto()
+        return True
+    else:
+        print("\nOpción incorrecta. Se espera un número entre 1 y 6\n")
+        return False
+
+def agregar_contacto():
+    nombre = input("Nombre: ")
+    direccion = input("Dirección: ")
+    telefono = input("Número de teléfono: ")
+    contactos[nombre] = {'Direccion': direccion, 'Telefono': telefono}
+    print("Contacto guardado!")
+
+verificacion = False
+while not verificacion:
+    mostrar_diccionario(menu)
+    valor = int(input("Elija una opción: "))
+    verificacion = comprobar_opcion(valor)
