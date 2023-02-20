ac2a12e2b999986e2708562455d4a0c5cc5ec444
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-02-20 08:23:18 -0500

    feat: tarea clase 5

---
+++ class-05/computadora.py
@@ -0,0 +1,36 @@
+class Computadora:
+    def __init__(self, marca: str, ram: int, tamano_disco: int, precio: float, stock: int, descuento: int = 0):
+        self.marca = marca
+        self.ram = ram
+        self.tamano_disco = tamano_disco
+        self.precio = precio
+        self.stock = stock
+        self.dscto = descuento
+
+    def __str__(self):
+        return f"Detalles de la computadora:\n \
+                Marca: {self.marca}\n \
+                Cantidad de RAM: {self.ram} GB\n \
+                Almacenamiento: {self.tamano_disco} GB\n \
+                Precio: USD {self.precio}\n \
+                Stock: {self.stock} unidades\n \
+                Precio (con descuento): USD {self.calcular_precio}\n"
+
+    #Asigna un nuevo precio (actualiza el precio)
+    def set_precio(self, nuevo_precio):
+        self.precio = nuevo_precio
+    
+    #Asigna un nuevo descuento (actualiza el descuento)
+    def set_dscto(self, nuevo_dscto):
+        self.dscto = nuevo_dscto
+
+    #Calcula precio con descuento
+    def calcular_precio(self):
+        return (self.precio * self.dscto) / 100
+
+    #Actualiza el stock (aumentándolo o disminuyéndolo según sea el parámetro)
+    def set_stock(self, variabilidad):
+        self.stock += variabilidad
+
+if __name__ == "__main__":
+    print("Ejecución del módulo computadora")
---
+++ class-05/main.py
@@ -0,0 +1,18 @@
+import computadora
+
+c1 = computadora.Computadora("Acer", 8, 125, 2600, 100)
+c2 = computadora.Computadora("Asus", 16, 256, 5200, 20, 10)
+
+print(c1)
+print(c2)
+
+c1.set_stock(5) #ingresan 5 computadoras al stock
+print(f"El stock es {c1.stock}") #verificamos que el stock ahora es 105
+c1.set_stock(-10) #se vendieron 10 computadoras
+print(f"El stock es {c1.stock}") #verificamos que el stock ahora es 95
+
+print(f"El descuento es: {c1.dscto}")   #verificamos que el descuento es de 0%
+print(f"El descuento es: {c2.dscto}")   #verificamos que el descuento es de 10%
+
+#intentamos conocer el precio con descuento => DA ERROR!!! :(
+print(c2.calcular_precio)
