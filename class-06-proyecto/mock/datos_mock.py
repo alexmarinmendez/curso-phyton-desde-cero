afb74d17d64ebd38ca911c306eddaed1c6a6541e
carlos8788 <carlos8788@gmail.com>
2023-02-26 11:30:17 -0300

    correcciones

--- class-06-proyecto/mock/datos_mock.py
+++ class-06-proyecto/mock/datos_mock.py
@@ -1,6 +1,7 @@
 from classes.profesor import Profesor
 
 #datos de ejemplo
+
 profesores_mock = {
     'D10123456': Profesor('Elizabeth', 'Bianchi', 'D10123456', 'Argentina', 26.9),
     'D20123456': Profesor('Fidenzio', 'Mazzi', 'D20123456', 'Argentina', 21.4),
--- class-06-proyecto/utils/crud_utils.py
+++ class-06-proyecto/utils/crud_utils.py
@@ -1,7 +1,7 @@
 from mock.datos_mock import profesores_mock
 from utils.data_utils import profesores
 from classes.profesor import Profesor
-
+import time
 def agregar():
     """Registra un profesor en la base de datos
     """
@@ -51,7 +51,9 @@ def mostrar():
     """Imprime la lista de profesores registrados
     """
     print("\nLista de profesores...")
-    print(profesores)
+    
+    for x,y  in profesores.items():
+        print(x,y)
     print("Fin de la lista de profesores.\n")
     input("Press <ENTER> to continue...")
 
@@ -70,12 +72,32 @@ def cargar_mock():
     """
     #pretendo que los datos de prueba se agreguen al diccionario (base de datos)
     #pero no funciona. Parece que 'profesores' lo tomara como variable local solo de esta funcion
-    profesores = profesores_mock
-    print(profesores)  #aquí me imprime los datos de prueba, pero no los datos agregados durante la ejecución del programa
-
+    # print(type(profesores_mock))
+    print("Cargando datos...")
+    # print(type(profesores))  #aquí me imprime los datos de prueba, pero no los datos agregados durante la ejecución del programa
+    time.sleep(2)
     #una posible solución: intento recorrer el diccionario de datos de ejemplo y asignarlo uno a uno al diccionario de profesores
     #pero ya no sé mas como hacerlo
-    # for x in profesores_mock.items():
-    #     print(x)
 
+    for x,y  in profesores_mock.items():
+        profesores[x]=y
+    # print(profesores, "printeando profesores")
+    # Solución
+
+    # for x,y  in profesores_mock.items():
+        # print(type(y))
+        
+        # profesores[x]={
+        #     'Nombres': y.nombres,
+        #     'Apellidos': y.apellidos, 
+        #     'DNI':y.dni, 
+        #     'Nacionalidad': y.nacionalidad, 
+        #     'Cantidad de horas trabajadas': y.cant_horas,
+        #     'Cotizacion': y.cotizacion, 
+        #     'Estado': y.activo
+        #     }
+        # print(x,y)
+    print("Se han cargado los datos!")
+    
+    # print(profesores, "printeando profesores")
     input("Press <ENTER> to continue...")
