a571d9a7b182bafedaab7c409bc612def248603a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-02-26 10:38:45 -0500

    feat: se agrego nuevos datos mockeados

--- class-06-proyecto/mock/datos_mock.py
+++ class-06-proyecto/mock/datos_mock.py
@@ -2,4 +2,10 @@ profesores_mock = {
     'D10123456': { 'nombres': 'Elizabeth', 'apellidos': 'Bianchi', 'dni': 'D10123456', 'nacionalidad': 'Argentina', 'cotizacion': 26.9 },
     'D20123456': { 'nombres': 'Fidenzio', 'apellidos': 'Mazzi', 'dni': 'D20123456', 'nacionalidad': 'Argentina', 'cotizacion': 21.4 },
     'D30123456': { 'nombres': 'Andrea', 'apellidos': 'Pinto', 'dni': 'D30123456', 'nacionalidad': 'Argentina', 'cotizacion': 22.0 },
+    'D40123456': { 'nombres': 'Maximiliana', 'apellidos': 'Aguilar ', 'dni': 'D40123456', 'nacionalidad': 'Peruana', 'cotizacion':  19.8 },
+    'D50123456': { 'nombres': 'Morgan', 'apellidos': 'Ballesteros', 'dni': 'D50123456', 'nacionalidad': 'Peruana', 'cotizacion':  21.4 },
+    'D60123456': { 'nombres': 'Vilfredo', 'apellidos': 'Raya', 'dni': 'D60123456', 'nacionalidad': 'Peruana', 'cotizacion':  22.2 },
+    'D70123456': { 'nombres': 'Anna', 'apellidos': 'Salas', 'dni': 'D70123456', 'nacionalidad': 'Peruana', 'cotizacion':  19.8 },
+    'D80123456': { 'nombres': 'Eliora', 'apellidos': 'Mondragón', 'dni': 'D80123456', 'nacionalidad': 'Peruana', 'cotizacion':  19.8 },
+    'D90123456': { 'nombres': 'Gregoria', 'apellidos': 'Galindo', 'dni': 'D90123456', 'nacionalidad': 'Peruana', 'cotizacion':  21.4 },
 }
